7a38b6d7799fcba748ee3e7b46005614ce33d4e4
Ubuntu <ubuntu@ip-172-31-34-68.eu-west-1.compute.internal>
2014-09-12 10:37:13 +0100

    Fixed name

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from distutils.core import setup
 setup(name='gmail_helper',
       version='1.0',
       description='Gmail helpers',
-      author='Greg Ward',
+      author='Stephen Raoul Sullivan',
       author_email='stephen@beyondspreadsheets.co.uk',
       url='https://github.com/raoulsullivan',
       #packages=['gmail_helper'],
