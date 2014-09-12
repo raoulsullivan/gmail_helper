c2e5de845535a387b5f82febf9d632ee8ee300de
Nestor Arocha <nesaro@gmail.com>
2014-09-12 10:26:54 +0100

    adds setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='gmail_helper',
+      version='1.0',
+      description='Gmail helpers',
+      author='Greg Ward',
+      author_email='stephen@beyondspreadsheets.co.uk',
+      url='https://github.com/raoulsullivan',
+      #packages=['gmail_helper'],
+      py_modules=['gmail_helper'],
+     )
